ca80cc92b49fe874f9e1a7f475d63a82dada1708
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-23 18:59:29 -0600

    App component updated

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,22 +1,59 @@
-import logo from "../../logo.svg";
 import "./App.scss";
+import MainPage from "../MainPage/index";
+import Work from "../Work/index";
+import { Route, Switch } from "react-router";
+import Grid from "../Grid";
+import WorkDetails from "../WorkDetails";
+import { useStore } from "../../store";
+import { Helmet } from "react-helmet";
 
 function App() {
+  const store = useStore((state) => state);
+
+  const sandboxItems = store.sandbox;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Cooper Terrones Design</title>
+      </Helmet>
+      <Switch>
+        <Route exact path="/work" render={() => <Work />} />
+        <Route
+          exact
+          path="/sandbox"
+          render={() => (
+            <Grid
+              header="This is my sandbox. A safe space to experiment and flex my imagination."
+              subheader="A place
+            to let my mind wander and grow."
+              items={sandboxItems}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/film"
+          render={() => (
+            <Grid
+              header="The beauty of film lies in the finite. The comfort in the imperfection."
+              subheader="Film captures the emotion of which a sensor could never recreate."></Grid>
+          )}
+        />
+        <Route exact path="/current-projects"></Route>
+        <Route
+          exact
+          path="/work-canoe-club"
+          render={() => <WorkDetails name="Canoe Club" />}
+        />
+        <Route
+          exact
+          path="/work-poppn-co"
+          render={() => <WorkDetails name="Poppn Co." />}
+        />
+        <Route exact path="/" render={() => <MainPage />} />
+      </Switch>
     </div>
   );
 }
