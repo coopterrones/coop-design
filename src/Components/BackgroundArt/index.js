22e9cb4119f0f63b88a4677736420229a93c947f
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-23 19:38:51 -0600

    Favicon updates

--- src/Components/BackgroundArt/index.js
+++ src/Components/BackgroundArt/index.js
@@ -5,7 +5,12 @@ import logo from "../../Assets/cooplogobg.png";
 const BackgroundArt = () => {
   return (
     <div className="background-container">
-      <img src={logo} height="400px" className="background-image" />
+      <img
+        src={logo}
+        height="400px"
+        className="background-image"
+        alt="coop logo background"
+      />
     </div>
   );
 };
