5340a24ec31a58573687e37cc69534660b3d3c18
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-25 03:15:06 -0600

    About me scroll working

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -17,14 +17,26 @@ const Nav = () => {
         onMouseEnter={() => updateLogo((logo) => (logo = cLogoLight))}
         onMouseLeave={() => updateLogo((logo) => (logo = cLogoDark))}>
         <img src={logo} height="50px" alt="c logo nav" />
-        <div className="active-dot-1 hidden"></div>
       </Link>
-      <a
+      <div className="about-me-scroll-container">
+        <div className="scroll-text">
+          Hello! 👋🏼🖐🏼👋🏼 I'm Coop. Originally from the San Francisco Bay Area.
+          Currently loving the Colorado lifestyle in Denver, CO. I am a
+          creative front end web developer looking to lend a unique perspective
+          to your biz. I love food, clothes, film photo, music, tech, etc...
+          show me something cool and I'll probably be interested and dive in.
+          Currently looking for my first Junior Role while putting in some time
+          at Canoe Club a menswear shop in Boulder, CO... Hope this finds you
+          well. "It's not what you don't know that kills you. It's what you know
+          for sure that just ain't so..." -Mark Twain ... ♥️♥️♥️
+        </div>
+      </div>
+      <Link
         href="mailto:cooper.terrones@outlook.com?Subject=Coop Design - get in touch"
         onMouseEnter={showMailCursor}
         onMouseLeave={hideMailCursor}>
         get in touch.
-      </a>
+      </Link>
     </div>
   );
 };
