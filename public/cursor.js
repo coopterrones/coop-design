9777e1e5c9300d651e117d22cbf88bee1a2e7f94
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-24 21:06:25 -0600

    Mail cursor showing on hover of get in touch link

--- public/cursor.js
+++ public/cursor.js
@@ -1,5 +1,6 @@
 let innerCursor = document.querySelector("#inner-cursor");
 let outterCursor = document.querySelector("#outter-cursor");
+let cursorText = document.querySelector("#cursor-text");
 
 document.addEventListener("mousemove", moveCursor);
 
@@ -11,4 +12,8 @@ function moveCursor(e) {
   innerCursor.style.top = `${y}px`;
   outterCursor.style.left = `${x}px`;
   outterCursor.style.top = `${y}px`;
+  cursorText.style.top = `${y}px`;
+  cursorText.style.left = `${x}px`;
 }
+
+// const showMailCursorText = () => {};
--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -4,6 +4,20 @@ import { Link } from "react-router-dom";
 import cLogoDark from "../../Assets/c-logo-black.png";
 import cLogoLight from "../../Assets/c-logo-light.png";
 
+const innerCursor = document.getElementById("inner-cursor");
+
+const showMailCursor = () => {
+  innerCursor.classList.add("mail-active");
+  innerCursor.classList.remove("inner-cursor");
+  innerCursor.innerText = "✉️";
+};
+
+const hideMailCursor = () => {
+  innerCursor.classList.add("inner-cursor");
+  innerCursor.classList.remove("mail-active");
+  innerCursor.innerText = "";
+};
+
 const Nav = () => {
   const [logo, updateLogo] = useState(cLogoDark);
 
@@ -17,7 +31,10 @@ const Nav = () => {
         <img src={logo} height="50px" alt="c logo nav" />
         <div className="active-dot-1 hidden"></div>
       </Link>
-      <a href="mailto:cooper.terrones@outlook.com?Subject=Hi Coop!">
+      <a
+        href="mailto:cooper.terrones@outlook.com?Subject=Coop Design - get in touch"
+        onMouseEnter={showMailCursor}
+        onMouseLeave={hideMailCursor}>
         get in touch.
       </a>
     </div>
