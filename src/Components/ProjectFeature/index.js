ab6a5a94931f72440ade48c96fc7bb94d15a592f
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-27 16:51:55 -0600

    Project feature component and scss updated

--- src/Components/ProjectFeature/index.js
+++ src/Components/ProjectFeature/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./ProjectFeature.scss";
 import { useStore } from "../../store";
 
+
 const ProjectFeature = ({ id }) => {
   const projects = useStore((state) => state.sandbox);
 
