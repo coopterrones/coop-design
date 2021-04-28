80db08421a69de66b1a37e7fd593a441247ea218
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-28 14:42:12 -0600

    Assets added for project pages

--- src/Components/ProjectFeature/index.js
+++ src/Components/ProjectFeature/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./ProjectFeature.scss";
 import { useStore } from "../../store";
+import GithubLogo from "../../Assets/Github-Logo.png";
 
 const ProjectFeature = ({ id }) => {
   const projects = useStore((state) => state.sandbox);
@@ -18,6 +19,14 @@ const ProjectFeature = ({ id }) => {
         playsInline
         src={project.gif}
         height="50%"></video>
+      <a href={project.link}>
+        <img
+          src={GithubLogo}
+          height="35px"
+          className="github-link-logo"
+          alt="github logo"
+        />
+      </a>
       <p className="project-feature-tech">{project.tech}</p>
       <p className="project-feature-description">{project.description}</p>
     </section>
--- src/store.js
+++ src/store.js
@@ -19,7 +19,8 @@ export const useStore = create((set) => ({
     {
       id: 2,
       name: "Favo",
-      description: "asdf",
+      description:
+        "Favo was created with volunteers in mind. The goal was to create an MVP that existed as a React app where users could browse volunteer opportunities and sign up. This project was completed over a 1 week sprint.",
       tech:
         "React, Hooks, ExpressJS, Cypress, Sass, ASYNC, React Testing Library, Jest",
       link: "https://github.com/coopterrones/favo",
@@ -28,7 +29,8 @@ export const useStore = create((set) => ({
     {
       id: 3,
       name: "Intention Timer",
-      description: "asdf",
+      description:
+        "Intenion timer is a vanilla JS exercise where users are able to set a category specific timer to block out time for task completion. This project was completed over a 5 day sprint.",
       tech: "Vanilla JavaScript, HTML, CSS",
       link: "https://github.com/coopterrones/intention-timer",
       gif: "https://media.giphy.com/media/8Ca6pTdQsuqSQSiigx/source.mp4",
@@ -36,7 +38,8 @@ export const useStore = create((set) => ({
     {
       id: 4,
       name: "What's Cookin",
-      description: "",
+      description:
+        "What's Cooking was an exercise using Vanilla JS. The main focus was to become confident in using network requests handling multiple datasets. Additionally, the practice was focused on using prototype iterator methods to handle those datasets.",
       tech: "Vanilla JavaScript, HTML, CSS, mocha, chai",
       link: "https://github.com/coopterrones/whats-cookin",
       gif: "https://media.giphy.com/media/rh80oLhh9IFPo6Xh8m/source.mp4",
@@ -44,7 +47,8 @@ export const useStore = create((set) => ({
     {
       id: 5,
       name: "Slapjack",
-      description: "asdf",
+      description:
+        "The Slapjack game was my first game development practice using vanilla JS. The focus was learning how to interact with the DOM and a big win was the use of local storage.",
       tech: "Vanilla JavaScript, HTML, CSS",
       link: "https://github.com/coopterrones/slapjack",
       gif: "https://media.giphy.com/media/NZ0TAOxwrj5RSDtUfV/source.mp4",
@@ -52,7 +56,8 @@ export const useStore = create((set) => ({
     {
       id: 6,
       name: "Overlook",
-      description: "asdf",
+      description:
+        "Overlook is a project that was intended to mimic the behavior of a hotel booking application. Although my UI is not traditional, my goal was to experiement with Neumorphism to see if it were a viable to use in a production type environment. Major wins on this project were the completion of functionality allowing users to login in as either admin or guests (not using real auth), and view their bookings, create and canel bookings for said user.",
       tech: "Vanilla JavaSript, HTML, CSS, ASYNC JS, Mocha, Chai",
       link: "https://github.com/coopterrones/Overlook",
       gif: "https://media.giphy.com/media/2aadUZzLa8sp5K8diw/source.mp4",
@@ -60,7 +65,8 @@ export const useStore = create((set) => ({
     {
       id: 7,
       name: "Rancid Tomatillos",
-      description: "asdf",
+      description:
+        "The rancid tomatillos app was a project that allowed users to browse a movie watch list and add to their watch list. This project was created using create-react-app . This project was intended to practice react, ASYNC JS, Cypress, and TDD. This project was completed over a 5 day sprint. ",
       tech: "React, Hooks, Sass, ASYNC, Cypress, React Testing Library, Jest",
       link: "https://github.com/coopterrones/rancid-tomatoes",
       gif: "https://media.giphy.com/media/FGP88kzB6HMpD3uQNG/source.mp4",
