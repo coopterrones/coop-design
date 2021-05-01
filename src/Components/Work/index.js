5fcfbe93f2b01171f656c9d741a67599cc493ea9
Cooper Terrones <cooper.terrones@outlook.com>
2021-05-01 17:19:00 -0600

    Work previews created working with sample "hi" text displaying correctly in preview

--- src/Components/Work/index.js
+++ src/Components/Work/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import FooterNav from "../FooterNav";
 import Nav from "../Nav";
 import { Link } from "react-router-dom";
@@ -6,19 +6,46 @@ import "./Work.scss";
 import SocialNav from "../SocialNav";
 
 const Work = () => {
+  const [previewLeftContent, setPreviewLeftContent] = useState(null);
+  const [previewRightContent, setPreviewRightContent] = useState(null);
+  const previewLeft = useRef(null);
+  const previewRight = useRef(null);
+
+  const displayWorkPreviewLeft = () => {
+    setPreviewLeftContent(<p> Hi </p>);
+  };
+
+  const displayWorkPreviewRight = () => {
+    setPreviewRightContent(<p>Hi</p>);
+  };
+
   return (
     <section className="work-page-container">
       <Nav />
+      <div className="work-preview-container">
+        <div className="work-preview-left" ref={previewLeft}>
+          {previewLeftContent}
+        </div>
+        <div className="work-preview-right" ref={previewRight}>
+          {previewRightContent}
+        </div>
+      </div>
       <div className="work-list-container">
         <ul className="work-list">
           <li className="work-list-item">
-            <Link className="list-link" to="/work-canoe-club">
+            <Link
+              className="list-link"
+              to="/work-canoe-club"
+              onMouseOver={displayWorkPreviewLeft}>
               <span>01.</span>
               <h2>Canoe Club</h2>
             </Link>
           </li>
           <li className="work-list-item">
-            <Link className="list-link" to="/work-poppn-co">
+            <Link
+              className="list-link"
+              to="/work-poppn-co"
+              onMouseOver={displayWorkPreviewRight}>
               <span>02.</span>
               <h2>Poppn Co.</h2>
             </Link>
