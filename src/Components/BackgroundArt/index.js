1d34aba27bdfe1568efd8d38800c743c7c3f57a3
Cooper Terrones <cooper.terrones@outlook.com>
2026-05-07 13:59:03 -0700

    feat: background logo movement

--- src/Components/BackgroundArt/index.js
+++ src/Components/BackgroundArt/index.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./BackgroundArt.scss";
 import logo from "../../Assets/cooplogobg.png";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, useMotionValue, useSpring } from "framer-motion";
 
 const BackgroundArt = () => {
+  const rawX = useMotionValue(0);
+  const rawY = useMotionValue(0);
+
+  const x = useSpring(rawX, { stiffness: 50, damping: 18 });
+  const y = useSpring(rawY, { stiffness: 50, damping: 18 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      rawX.set((e.clientX / window.innerWidth - 0.5) * 40);
+      rawY.set((e.clientY / window.innerHeight - 0.5) * 40);
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, [rawX, rawY]);
+
   return (
     <AnimatePresence exitBeforeEnter>
       <motion.div
@@ -12,11 +27,12 @@ const BackgroundArt = () => {
         transition={{ type: "tween", duration: 0.75 }}
         exit={{ opacity: 0 }}
         className="background-container">
-        <img
+        <motion.img
           src={logo}
           height="400px"
           className="background-image"
           alt="coop logo background"
+          style={{ x, y }}
         />
       </motion.div>
     </AnimatePresence>
